755374d1c459539f436a13ffa124ed37e3955705
niuzhuo <newjoyde@gmail.com>
2019-04-02 23:29:57 +0200

    add a new file iss01_add.cpp

---
+++ iss01_add.cpp
@@ -0,0 +1,6 @@
+// This file shows how to build a new branch and finally merge
+// Now Summary:
+// 1. git checkout -b iss01		--git branch iss01(build a new branch) && git checkout iss01(switch to iss01)
+// 2. do some works. example: edit a new file	--edit in a new branch
+// 3. git checkout origin		--go back to origin and edit
+// 4. merge
