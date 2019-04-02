38d867d9ef4a2a69651f1c83447fa35b0ef4b771
Newjoy <38489219+newjoy2018@users.noreply.github.com>
2019-04-02 22:04:54 +0200

    change the file in Github

--- pushtest.cpp
+++ pushtest.cpp
@@ -6,3 +6,7 @@
 // 1. edit a file		---check git status: Untracked files(Red)
 // 2. git add [file name]	---check git status: Changes to be committed(Green)
 // 3. git commit -m "why to commit"	---commit arbitray number of files whenever they are added
+
+// This is the first time that directly change a file in Github
+// Try to see what differences between master and origin
+// At the same time, pull
