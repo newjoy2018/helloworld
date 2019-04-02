83b005a9bbb5b7a2c2fc5093b5a4fcb7bb689933
niuzhuo <newjoyde@gmail.com>
2019-04-02 21:58:42 +0200

    2.commit add the first part of git Summary

--- pushtest.cpp
+++ pushtest.cpp
@@ -1,2 +1,8 @@
 // This file try to use add/commit/push 
 // fist try
+
+// first git add / commit / push is success
+// Summary: 
+// 1. edit a file		---check git status: Untracked files(Red)
+// 2. git add [file name]	---check git status: Changes to be committed(Green)
+// 3. git commit -m "why to commit"	---commit arbitray number of files whenever they are added
